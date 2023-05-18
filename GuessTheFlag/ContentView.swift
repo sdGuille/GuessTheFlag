edfeccfc32690f144d97483d78c5366083480528
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-05-18 17:36:48 -0600

    Using stacks to arrange views

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            Spacer()
             Text("Hello, world!")
+            Text("This is another text view")
+            Text("This is inside a stack")
+            Spacer()
+            Spacer()
         }
-        .padding()
     }
 }
 
