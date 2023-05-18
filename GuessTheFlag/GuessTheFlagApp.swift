383e2ab062b4b191b1196761934877799726a5ed
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-05-18 17:22:29 -0600

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by MacBook Pro on 18/5/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
