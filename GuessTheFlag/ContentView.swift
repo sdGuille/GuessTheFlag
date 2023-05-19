9ff334e59caebb8d0ccc7e107a22baffc8c225ca
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-05-19 13:22:43 -0600

    Alert and more functionality on buttons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,22 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
         ZStack {
             VStack {
-                Button("Button 1") { }
-                    .buttonStyle(.bordered)
-                Button("Button 2", role: .destructive) { }
-                    .buttonStyle(.bordered)
-                Button("Button1") { }
-                    .buttonStyle(.borderedProminent)
-                    .tint(.teal)
-                Button("Button1", role: .destructive) { }
-                    .buttonStyle(.borderedProminent)
-                Button {
-                    print("Tap this button")
-                } label: {
-                    Image(systemName: "pencil")
+                Button("Show Alert") {
+                    showingAlert = true
+                }
+                .alert("Important message", isPresented: $showingAlert) {
+                    Button("Delete", role: .destructive) { }
+                    Button("Cancel", role: .cancel) { }
+                } message: {
+                    Text("Please read this")
                 }
             }
         }
